371b793d3ea424bbdf46ab04df20f40e6479fc6d
tongjch <jiachtong@163.com>
2018-03-28 23:08:48 +0800

    打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
@@ -28,7 +29,7 @@ module.exports = {
             {
                 test: /\.css$/, loader: ExtractTextPlugin.extract({
                 fallback: "style-loader",
-                use: "css-loader"
+                use: "css-loader?minimize"
             })
             },
             {
@@ -42,6 +43,24 @@ module.exports = {
         ]
     },
     plugins: [HtmlWebpackPluginConfig,
+        new UglifyJSPlugin({
+            compress: {
+                // 在UglifyJs删除没有用到的代码时不输出警告
+                warnings: false,
+                // 删除所有的 `console` 语句，可以兼容ie浏览器
+                drop_console: true,
+                // 内嵌定义了但是只用到一次的变量
+                collapse_vars: true,
+                // 提取出出现多次但是没有定义成变量去引用的静态值
+                reduce_vars: true,
+            },
+            output: {
+                // 最紧凑的输出
+                beautify: false,
+                // 删除所有的注释
+                comments: false,
+            }
+        }),
         new ExtractTextPlugin('dist/css/[name].css'),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
