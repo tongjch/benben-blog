23b3c863795d15163a492d9166388da199cea48f
benben <jiachtong@163.com>
2018-03-28 19:24:54 +0800

    菜单设计

--- src/header/Header.js
+++ src/header/Header.js
@@ -8,6 +8,21 @@ class Header extends Component {
             <div className="header-container">
                 <div className="header-center-container">
                     <div className="header-author">BenBen Blog</div>
+                    <div className="header-menu-container">
+                        <ul className="header-menu">
+                            <li className="header-menu-item">
+                                <i className="fa fa-home fa-fw"></i>
+                                首页
+                            </li>
+                            <li className="header-menu-item">
+                            <i className="fa fa-tags fa-fw"></i>
+                                标签
+                            </li>
+                            <li className="header-menu-item">分类</li>
+                            <li className="header-menu-item">归档</li>
+                            <li className="header-menu-item">搜索</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Layout from './layout/Layout'
+import Layout from './layout/Layout';
+require("../node_modules/font-awesome/css/font-awesome.css");
 
 
 render((
