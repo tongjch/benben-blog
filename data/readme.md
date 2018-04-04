a9a1e3c2b8d03a84e5d44660f78edeb148a58700
Morning <jiachtong@163.com>
2018-04-04 23:40:08 +0800

    打包工具修改

---
+++ data/readme.md
@@ -0,0 +1,3 @@
+### data.json 存储了所有blog数据
+### last.json 存储了最近的12条blog
+### tags.json 存储了所有根据tag分类的文章
--- src/tools/deploy.js
+++ src/tools/deploy.js
@@ -6,107 +6,80 @@ const lineReader = require('line-reader')
 let moment = require('moment')
 
 console.log("Welcome to use benben blog deploy tools");
-console.log("Version:","0.0.1");
+console.log("Version:","0.0.2");
 console.log("base dir:", __dirname);
-
-let args = process.argv;
-if(args.length < 1){
-    console.log("Illegal Args!!!!")
-    process.exit(1);
-}
-let lastArg = args[args.length-1]
-
-let extname = path.extname(lastArg)
-if(extname != ".md" && extname != ".MD"){
-    console.log("Illegal Arg:",lastArg);
-    console.log("Please enter markdown file")
-    process.exit(1);
-}
-
-console.log("start parse markdown file:",lastArg);
-
-let exist = fs.existsSync(lastArg);
-if(!exist){
-    lastArg = path.join(__dirname,lastArg);
-    exist = fs.existsSync(lastArg);
-}
-
-if(!exist){
-    console.log(lastArg,"Markdown file not exists!!!");
-    process.exit(1);
-}
+let jsonFile = "deploy/deploy.json";
 
 let meta = {
     id: new Date().getTime(),
     time: moment().format("YYYY-MM-DD")
 }
-//{"id": 1,"name": "区块链历史2","time": "2018-03-30 09:35:00",
-//"tags": ["区块链"],
-//"file": "/articles/2018-03-29/blockchain_history.md",
-//"desc": "区块链历史简介2"}
 
-function parseLine(line){
-    if(!line.startsWith(";")){
-        return;
-    }
-    
-    line = line.substring(1).trim();
-    console.log(line)
-    if(line.startsWith("name:")){
-        line = line.substring(5).trim();
-        meta.name = line;
-    }else if(line.startsWith("tags:")){
-        line = line.substring(5).trim();
-        meta.tags = line.split("|");
-    }else if(line.startsWith("desc:")){
-        line = line.substring(5).trim();
-        meta.desc = line;
-    }
-}
-
-new Promise(function(resolve,reject){
-    lineReader.eachLine(lastArg,function(line, last, cb){
-        parseLine(line);
-        if(meta.tags && meta.desc && meta.name){
-            cb(false);
-            resolve();
-        }else{
-            cb()
-        }
-    })
-}).then(function(){
-    meta.file = "/articles/"+meta.time+"/"+path.basename(lastArg);
-    console.log("parse meta info succ!!",meta);
-    storeLastData(meta)
-    storeAll(meta)
-    moveMarkDownFile(meta);
-})
-
-//获取last.json文件中的数据
-function storeLastData(meta){
-  var content = fs.readFileSync("data/last.json","UTF-8");
-  var json = JSON.parse(content);
-  json.unshift(meta);
-  //最近版本最多只能存储12个
-  if(json.length > 12){
-      json.pop();
-  }
-  fs.writeFileSync("data/last.json",JSON.stringify(json),"UTF-8");
-}
+let json = JSON.parse(fs.readFileSync(jsonFile,"UTF-8"));
+console.log("read deploy.json file:",json);
 
 //移动markdown文件
-function moveMarkDownFile(meta){
-    var file = meta.file.substring("1");
-    if(!fs.existsSync(path.dirname(file))){
-        fs.mkdirSync(path.dirname(file))
+moveFile("deploy/"+json.file+".md");
+//移动html文件
+moveFile("deploy/"+json.file+".html");
+
+//赋值meta信息
+meta.name = json.name;
+meta.desc = json.desc;
+meta.tags = json.tags;
+meta.file = "/articles/"+meta.time+"/"+json.file+".html";
+
+//存储last.json数据
+let lastFile = "data/last.json";
+let last = JSON.parse(fs.readFileSync(lastFile,"UTF-8"));
+last.unshift(meta);
+if(last.length > 12){
+    console.log("last length > 12 will remove last")
+    meta.pop();
+}
+fs.writeFileSync(lastFile,JSON.stringify(last),"UTF-8");
+
+//存储data.json数据
+ let dataFile = "data/data.json";
+ let all = JSON.parse(fs.readFileSync(dataFile,"UTF-8"));
+ all.push(meta);
+ fs.writeFileSync(dataFile,JSON.stringify(all),"UTF-8");
+//存储tag.json数据
+let tagFile = "data/tags.json";
+let tagJSON = JSON.parse(fs.readFileSync(tagFile,"UTF-8"));
+for(let tag of json.tags){
+    if(tag == undefined || tag == null || tag.trim() == ""){
+        continue;
     }
-    fs.writeFileSync(file,fs.readFileSync(lastArg));
-    fs.unlinkSync(lastArg);
+    let arr = tagJSON[tag];
+    if(arr == undefined || arr == null){
+        arr = [];
+        tagJSON[tag] = arr;
+    }
+    arr.push(meta);
+}
+fs.writeFileSync(tagFile,JSON.stringify(tagJSON),"UTF-8");
+
+
+//移动文件工具
+function moveFile(file){
+    console.log("starting move file:",file);
+    if(!fs.existsSync(file)){
+        console.error(file,"file not exists!!!")
+        throw new Error("file not exists");
+    }
+    var base = "articles/"+meta.time;
+    if(!fs.existsSync(base)){
+        fs.mkdirSync(base);
+    }
+
+    var content = fs.readFileSync(file,"UTF-8");
+
+    fs.writeFileSync(base+"/"+path.basename(file),content,"UTF-8");
+
+    fs.unlinkSync(file);
+
+    console.log("end move file",file);
 }
 
-function storeAll(meta){
-    var content = fs.readFileSync("data/data.json","UTF-8");
-    var json = JSON.parse(content);
-    json.push(meta);
-    fs.writeFileSync("data/data.json",JSON.stringify(json),"UTF-8");
-}
+
